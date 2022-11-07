b5e0c631facb5d1b34a9ee30cc179c039d8be586
kryliga <117622886+kryliga@users.noreply.github.com>
2022-11-07 15:34:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ox5UI6Mkso2GcFGTqZEq_GtBHmlQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'raRyo7HfOcLSkv7D7CjF6IsnbLnTM-3WOk4ykqz5AoQ',
+      useTemplateId: 'Va7t2wMAMBhSYoMqj4BkxgzRy9p4r3-PbUHnSDI_ehE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
