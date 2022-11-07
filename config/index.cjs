4fa8f68ecbd9820adeb8276053c94df3446b91ee
kryliga <117622886+kryliga@users.noreply.github.com>
2022-11-07 16:07:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,32 +19,32 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '汪师傅',
+      name: '佳佳的圈外女友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ox5UI6Mkso2GcFGTqZEq_GtBHmlQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Va7t2wMAMBhSYoMqj4BkxgzRy9p4r3-PbUHnSDI_ehE',
+      useTemplateId: '67fCgFk9Nd3uPMgDNNE0wWLe2c5uDaOoHr5H532rZbg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-15',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小汪同学', year: '1998', date: '12-21',
+          type: '*生日', name: '小苏同学', year: '20000', date: '10-20',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '汪师傅', year: '1999', date: '02-06',
+          type: '生日', name: '小苏同学', year: '2000', date: '11-15',
         },
         {
-          type: '节日', name: '遇见', year: '2018', date: '09-12',
+          type: '节日', name: '纪念日', year: '2022', date: '08-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
-        // 成为朋友的日子
-        { keyword: 'love_day', date: '2019-06-06' },
-        // 距离上次见面
-        { keyword: 'marry_day', date: '2022-06-05' },
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-08-15' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-08-31' },
       ],
     },
   ],
